c626440e9800d378d82ef21bca5dd45f79991fea
varunuppala <vu14@georgetown.edu>
2021-09-19 23:07:26 -0400

    Step 1

--- main.py
+++ main.py
@@ -1,53 +1,80 @@
 '''
 # TODO: searchEngine
-
+---> enter the filename and get the tokens out of it.
+---> Get all the stop words to ignore them from the document.
+---> tokenize the documents using the stop words list.
 '''
 
 
+#-------------import package/module----------------------------#
+#
 import argparse
 import os
 import sys
+import re
 
-def identifyTokens(s):
-	``
-
-
-
-
-
-
-
-"""def checkParse():
-	# Create the parser
-	my_parser = argparse.ArgumentParser(description='List the content of a folder')
-
-	# Add the arguments
-	my_parser.add_argument('Path',
-	                       metavar='path',
-	                       type=str,
-	                       help='the path to list')
-
-	# Execute the parse_args() method
-	args = my_parser.parse_args()
-
-	input_path = args.Path
-
-	if not os.path.isdir(input_path):
-	    print('The path specified does not exist')
-	    sys.exit()
-
-	print('\n'.join(os.listdir(input_path)))
-"""
+#---------------- Validate Rows---------------------#
+def validateLine():
+	document = readFile()
+	# for comment lines
+	comment = re.compile(r"^<!--[ a-zA-Z0-9 =\/]*-->$")
+	# for Blank Spaces
+	#blank = re.compile(r"[\s]+")
+	for i in document:
+		match = comment.search(i)
+		#match1 = blank.search(i)
+		if match:
+			pass
+		else:
+			print(i.split(' '))
 
 
-#Main function for all the calls to be made program
+#----------------Loading all the stopwords to check further-------------------------#
+
+def getStopWords():
+	stopWords = {}
+	with open("stops.txt") as f:
+		for line in f:
+			val = line.split()
+			stopWords[val[0]] = 1
+	return stopWords
+
+#--------------to check the document lines----------------------------#
+
+def identifyTokens():
+	stopWords=getStopWords()
+	document = readFile()
+	#stroing document numbers
+	documents = {}
+	doc_re = re.compile(r'<DOCNO>.([A-Za-z_-][A-Za-z0-9_-]*).<\/DOCNO>')
+	for i in document:
+		match = doc_re.search(i)
+		if match:
+			documents[len(documents)+1] = match.group(1)
+	for i in documents.values():
+		print(i)
+
+
+
+
+
+
+
+
+#----------------To read and yield each line of a file-------------------------#
+def readFile():
+	for row in open("example.txt", "r"):
+		yield row
+
+#-----------------Main function for all the calls to be made program------------------------#
+
 def main():
-	s = input("enter a string")
-	identifyTokens(s)
-	print("hello world")
+	#s = input("enter a string")
+	#readFile()
+	validateLine()
+
+#---------------------to call the main function----------------------------#
 
 
-
-# to call the main function
 if __name__ == "__main__":
 	main()
--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-s = " hi there hello {Pkllakshld,ajhfljadf}"
+def sum1(n):
+    s = 0
+    for i in str(n):
+        s+=int(i)
+    return s
 
-hihihi
-print(s[s.find('{')+1:s.find('}')].split(','))
+
+
+def sum(n):
+    c = 0
+    s = 0
+    while n > 0:
+        curr = n %10
+        if c%2 == 0:
+            s = s + curr
+        else:
+            s += sum1(curr*2)
+        c +=1
+        n = n//10
+    return s
+
+print(sum(int(input("enter number"))))
