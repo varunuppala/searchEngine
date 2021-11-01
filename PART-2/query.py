549a81b86046701bf5b745e32e77928d9f289c60
varunuppala <vu14@georgetown.edu>
2021-11-01 19:17:04 -0400

    added formula

--- PART-2/query.py
+++ PART-2/query.py
@@ -10,6 +10,8 @@ import time
 import querydoc as qd
 import warnings
 from pprint import pprint
+import json
+import pandas as pd
 
 warnings.filterwarnings('ignore')
 
@@ -43,10 +45,46 @@ def main():
     #indexPath,queryPath,retrievalMode,indexPath,resultsPath = parse_arguments()
     #print(indexPath,queryPath,retrievalMode,indexPath,resultsPath)
     queries = qd.main()# pass the queryPath to this in the later stage
-    queryUpdated = qd.phraseindexQuery(queries)
+    queryUpdated = qd.singleindexQuery(queries)
     pprint(queryUpdated)
 
+    f = open("../PART-1/Output/final.json",)
+    f2 = open("../PART-1/Output/lexicon.json",)
+    f3 = open("../PART-1/Output/documentlist.json",)
 
+    invertedindex = json.load(f)
+    lexicon = json.load(f2)
+    doclist = json.load(f3)
+    method = 'lm'
+    df = pd.DataFrame(columns = ['queryid', '0', 'docid', 'rank', 'score', 'method'])
+    numq = 0
+    for queryid, querytermlist in queryUpdated.items():
+        dict1 = {}
+        for queryterm in querytermlist:
+            for docid, tf in invertedindex[queryterm].items():
+                if method == 'cos':
+                    sim = tf*(len(doclist)/lexicon[queryterm])*(len(doclist)/lexicon[queryterm])
+                elif method == 'bm':
+                    sim = 2.2*tf*((len(doclist) - lexicon[queryterm])/lexicon[queryterm])/(tf + 1.2*(0.25 + 0.75*1))
+                elif method == 'lm':
+                    sim = (invertedindex[queryterm][docid] + 0.2*tf/len(doclist))/(220 + 0.2)
+                if docid in dict1:
+                    dict1[docid] += sim
+                else:
+                    dict1[docid] = sim
+
+        dict1 = sorted(dict1.items(), key=lambda x: x[1], reverse=True)
+
+        rank = 0
+        for docid, score in dict1:
+            rank += 1
+            print(str(queryid) + ' 0 ' + str(doclist[docid]) + ' ' + str(rank) + ' ' + str(score) + ' ' + method)        
+            if rank == 100:
+                break
+        numq += 1
+        if numq == 5:
+            exit()
+        
 
 
 
