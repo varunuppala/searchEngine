8ffcd745532804e85cd3dfe2c7c25b14a74b3676
varunuppala <vu14@georgetown.edu>
2021-10-31 22:32:44 -0400

    query processing done

--- PART-2/query.py
+++ PART-2/query.py
@@ -7,6 +7,12 @@ To-do Query processing
 import argparse
 import os
 import time
+import querydoc as qd
+import warnings
+from pprint import pprint
+
+warnings.filterwarnings('ignore')
+
 
 def parse_arguments():
     """
@@ -26,17 +32,21 @@ def main():
     """
     main function for the script
     Goals:
-    1.Argument Parsing
+    1.Argument Parsing done
     2.Assigns calls as specified by the user.(Can be done later)
     3.Build indexes for the query document
     4.Load the indexes from the previous step(Part-1)
     5.Compare using technqiues
     6.Report them
-
     """
-    start_time = time.time()
+    start_time = time.time()#tracking time
     #indexPath,queryPath,retrievalMode,indexPath,resultsPath = parse_arguments()
     #print(indexPath,queryPath,retrievalMode,indexPath,resultsPath)
+    queries = qd.main()# pass the queryPath to this in the later stage
+    queryUpdated = qd.phraseindexQuery(queries)
+    pprint(queryUpdated)
+
+
 
 
 
